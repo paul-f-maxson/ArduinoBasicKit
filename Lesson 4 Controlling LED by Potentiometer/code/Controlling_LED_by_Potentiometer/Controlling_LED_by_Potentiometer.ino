0df8bf28560352ae21d396a82469da3b5b6a6c58
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-12 14:52:27 -0500

    Updates

--- Lesson 4 Controlling LED by Potentiometer/code/Controlling_LED_by_Potentiometer/Controlling_LED_by_Potentiometer.ino
+++ Lesson 4 Controlling LED by Potentiometer/code/Controlling_LED_by_Potentiometer/Controlling_LED_by_Potentiometer.ino
@@ -4,10 +4,10 @@
 //Website:www.sunfounder.com
 //2015.5.7
 /******************************************/
-const int analogPin = A0;//the analog input pin attach to
-const int ledPin = 9;//the led attach to
-int inputValue = 0;//variable to store the value coming from sensor
-int outputValue = 0;//variable to store the output value
+const int analogPin = A0; //the analog input pin attach to
+const int ledPin = 9;     //the led attach to
+int inputValue = 0;       //variable to store the value coming from sensor
+int outputValue = 0;      //variable to store the output value
 /******************************************/
 void setup()
 {
@@ -15,8 +15,8 @@ void setup()
 /******************************************/
 void loop()
 {
-  inputValue = analogRead(analogPin);//read the value from the sensor
-  outputValue = map(inputValue,0,1023,0,255);//Convert from 0-1023 proportional to the number of a number of from 0 to 255
-  analogWrite(ledPin,outputValue);//turn the led on depend on the output value
+  inputValue = analogRead(analogPin);             //read the value from the sensor
+  outputValue = map(inputValue, 0, 1023, 0, 255); //Convert from 0-1023 proportional to the number of a number of from 0 to 255
+  analogWrite(ledPin, outputValue);               //turn the led on depend on the output value
 }
 /*******************************************/
